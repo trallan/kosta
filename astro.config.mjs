15b07f2890fc0e20927561a63b989f572e2b9ac4
Viktor Erlandsson <72042504+trallan@users.noreply.github.com>
2025-06-04 17:27:50 +0000

    Updated config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://trallan.github.io',
-  base: '/',
+  site: 'https://trallan.github.io/kosta.github.io/',
+  base: '/kosta.github.io/',
 
   vite: {
     plugins: [tailwindcss()],
