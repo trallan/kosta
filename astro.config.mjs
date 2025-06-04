a41dea4535653f601f22c9192db4f50327138e63
Viktor Erlandsson <72042504+trallan@users.noreply.github.com>
2025-06-04 13:47:32 +0000

    Added Nav, Footer and Body

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://trallan.github.io',
+  site: 'https://trallan.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
